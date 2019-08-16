4613cdf18a3dd6245e54fb997b0470c147bc86c1
yhwork <878507710@qq.com>
2019-08-16 21:29:25 +0800

    1
    1

--- config/index.js
+++ config/index.js
@@ -13,8 +13,8 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/store':{
-        target: 'http://192.168.1.100:8083',
-        // target: 'https://qa.xdb.iforbao.com/store/',
+        // target: 'http://192.168.1.100:8083',
+        target: 'https://qa.xdb.iforbao.com/store/',
         // target: 'https://xdb.iforbao.com/store/',
         changeOrigin:true,
         pathRewrite:{
@@ -24,8 +24,8 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: '192.168.1.105', // can be overwritten by process.env.HOST
-    // host:'192.168.1.104',
+    // host: '192.168.1.105', // can be overwritten by process.env.HOST
+    host:'127.0.0.1',
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
