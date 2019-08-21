ca7389f60887e9381da4c5c5dbe1fdebe63bf21b
yhwork <878507710@qq.com>
2019-08-21 19:29:43 +0800

    员工管理
    员工管理

---
+++ src/components/page/educationalCenter/usermge.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        员工管理
+    </div>
+</template>
+<script>
+export default {
+    name:'usermge',
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
